5f1325a72811a901c5a2cc710eecfaa8c8c2221f
Yousaf Nabi <you@saf.dev>
2023-09-05 00:41:14 +0100

    feat: ocran plus pact-broker

--- packaging/pact-provider-verifier.rb
+++ packaging/pact-provider-verifier.rb
@@ -29,5 +29,10 @@ class Thor
   end
 end
 
+# Ocran will allow use to include our own cert file at packaging time
+# this points to the CA cert bundle take from traveling-ruby
+if (ENV['OCRAN_EXECUTABLE'] || ENV['OCRAN_EXECUTABLE'] != '') && ENV['SSL_CERT_FILE'].nil?
+  ENV['SSL_CERT_FILE'] = File.join(File.dirname($0), 'ca-bundle.crt')
+end
 
 Pact::ProviderVerifier::CLI::Verify.start
