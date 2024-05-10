169120d150e2b1c207a9d78275650c5148e92ac2
Yousaf Nabi <you@saf.dev>
2024-05-10 02:14:11 +0100

    skip plugin-cli test on musl as cli not included

--- script/test.sh
+++ script/test.sh
@@ -1,12 +1,12 @@
-#!/bin/bash -eu
-set -eu # This needs to be here for windows bash, which doesn't read the #! line above
+#!/bin/bash -e
+set -e # This needs to be here for windows bash, which doesn't read the #! line above
 
 detected_os=$(uname -sm)
 echo detected_os = $detected_os
 BINARY_OS=${BINARY_OS:-}
 BINARY_ARCH=${BINARY_ARCH:-}
 FILE_EXT=${FILE_EXT:-}
-ALPINE=$(cat /etc/os-release | grep -i alpine || true)
+ALPINE=$(cat /etc/os-release | grep -i alpine &>/dev/null && echo true || false)
 if [ "$BINARY_OS" == "" ] || [ "$BINARY_ARCH" == "" ] ; then 
     case ${detected_os} in
     'Darwin arm64')
@@ -85,7 +85,7 @@ for tool in ${tools[@]}; do
   if [ "$BINARY_OS" != "windows" ] ; then echo "no bat file ext needed for $(uname -a)" ; else FILE_EXT=.bat; fi
   if [ "$BINARY_OS" = "windows" ] && [ "$tool" = "pact-plugin-cli" ] ; then  FILE_EXT=.exe ; else echo "no exe file ext needed for $(uname -a)"; fi
   echo executing ${tool}${FILE_EXT} 
-  if [ "$BINARY_ARCH" = "x86" ] && [ "$tool" = "pact-plugin-cli" ] || [ "$ALPINE" = true ] && [ "$tool" = "pact-plugin-cli" ] ; then  echo "skipping for x86" ; else ${tool}${FILE_EXT} help; fi
+  if [ "$BINARY_ARCH" = "x86" ] && [ "$tool" = "pact-plugin-cli" ] || [ "$ALPINE" = true ] && [ "$tool" = "pact-plugin-cli" ] ; then  echo "skipping for x86" ; else $PATH_TO_BIN/${tool}${FILE_EXT} help; fi
 done
 
 
